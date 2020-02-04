a57b915cd380fdd0b40cbe14be8486f0719b8239
Michael Mendoza <michael.mend97@yahoo.com>
2020-02-04 15:44:10 -0500

    styling changes

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,25 @@
-$('#srchbtn').click(function(){
-    var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?id=524901&appid=06e67d1c01fd425c507533b8a4c46d90&q=' + srchbx.value;
+$('#date').html(moment().format(' MMMM Do YYYY'))
 
-    console.log(queryUrl);
+$('#srchbtn').click(function(){
+    var cwQueryUrl = 'https://api.openweathermap.org/data/2.5/weather?appid=06e67d1c01fd425c507533b8a4c46d90&q=' + srchbx.value;
+    var fdQueryUrl = 'https://api.openweathermap.org/data/2.5/forecast?appid=06e67d1c01fd425c507533b8a4c46d90&q=' + srchbx.value;
 
     $.ajax({
-        url: queryUrl,
+        url: cwQueryUrl,
         method: 'get'
     }).then(function(res) {
-        $('#city').html(res.name);
-        $('#temp').html(Math.floor(res.main.temp * 1.8 - 459.76));
+        console.log(res);
+        $('#city').html(res.name + ', ' + res.sys.country);
+        $('#temp').html(Math.floor(res.main.temp * 1.8 - 459.76) + ' &#176;F');
         $('#humid').html(res.main.humidity + '%');
         $('#wind').html(res.wind.speed + ' mph');
-        $('#uv').html();
-        console.log(srchbx.value)
-        console.log(res)
-        console.log(res.name)
     });
+    $.ajax({
+        url: fdQueryUrl,
+        method: 'get'
+    }).then(function(res) {
+        console.log(moment().format('D') + 1);
+        console.log(res.list[6].main.humidity);
+        console.log(Math.floor(res.list[6].main.temp * 1.8 -459.76));
+    })
 });
